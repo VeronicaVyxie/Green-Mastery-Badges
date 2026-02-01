50bb8d69edcee15126fce81d6ae55a157cbe2212
Veronica <61025394+VeronicaVyxie@users.noreply.github.com>
2026-02-01 11:46:37 +0100

    Add files via upload

---
+++ Green Mastery Borders.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Green Mastery Borders
+// @namespace    RetroAchievements
+// @version      1.0.0
+// @description  green :3
+// @author       VeronicaVyxie
+// @match        https://retroachievements.org/user/*
+// @exclude      https://retroachievements.org/user/*/progress
+// @exclude      https://retroachievements.org/user/*/posts
+// @exclude      https://retroachievements.org/user/*/games
+// @exclude      https://retroachievements.org/user/*/developer
+// @exclude      https://retroachievements.org/user/*/tickets
+// @run-at       document-start
+// @icon         https://static.retroachievements.org/assets/images/favicon.webp
+// @grant    GM_addStyle
+// ==/UserScript==
+
+GM_addStyle ( `
+    .goldimage {
+        border:2px solid green !important;
+    }
+` );
